790a7a8d3f8846594ae9c91f598ccedb112104fb
Patryk Szela <53914832+PatrickSzela@users.noreply.github.com>
2024-03-13 15:05:19 +0100

    chore: remove storybook onboarding

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,11 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: [
-    "../**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/nextjs",
