c78c11cdd99cca300988d19c03377495072a6c94
Patryk Szela <53914832+PatrickSzela@users.noreply.github.com>
2024-09-26 22:05:53 +0200

    fix: duplicates when grouping references by sheet

--- libs/sheetflow/sheet.ts
+++ libs/sheetflow/sheet.ts
@@ -1,7 +1,7 @@
 import { CellContent } from "./cell";
 import { buildCellAddress, CellAddress, isCellAddress } from "./cellAddress";
 import { isCellRange } from "./cellRange";
-import { NamedExpressions } from "./namedExpression";
+import { NamedExpression, NamedExpressions } from "./namedExpression";
 import { Reference, SheetFlow } from "./sheetflow";
 
 export type Sheet = CellContent[][];
@@ -17,9 +17,17 @@ export type GroupedCells = Record<
 export const groupReferencesBySheet = (
   sf: SheetFlow,
   references: Reference[]
-) => {
-  const cells: GroupedCells = {};
-  const namedExpressions: NamedExpressions = [];
+): {
+  cells: GroupedCells;
+  namedExpressions: NamedExpressions;
+  missingSheets: string[];
+  missingNamedExpressions: string[];
+} => {
+  const cells: Record<
+    string,
+    Record<string, GroupedCells[string][number]>
+  > = {};
+  const namedExpressions: Record<string, NamedExpression> = {};
 
   const missingSheets: Set<string> = new Set();
   const missingNamedExpressions: Set<string> = new Set();
@@ -31,7 +39,7 @@ export const groupReferencesBySheet = (
         continue;
       }
 
-      namedExpressions.push(sf.getNamedExpression(precedent));
+      namedExpressions[precedent] = sf.getNamedExpression(precedent);
     } else if (isCellAddress(precedent)) {
       const { sheet } = precedent;
 
@@ -40,15 +48,15 @@ export const groupReferencesBySheet = (
         continue;
       }
 
-      if (cells[sheet] === undefined) cells[sheet] = [];
+      if (cells[sheet] === undefined) cells[sheet] = {};
 
       const stringAddress = sf.cellAddressToString(precedent);
 
-      cells[sheet].push({
+      cells[sheet][stringAddress] = {
         address: precedent,
         stringAddress,
         content: sf.getCell(precedent),
-      });
+      };
     } else if (isCellRange(precedent)) {
       const { start, end } = precedent;
       const { sheet } = start;
@@ -58,25 +66,30 @@ export const groupReferencesBySheet = (
         continue;
       }
 
-      if (cells[sheet] === undefined) cells[sheet] = [];
+      if (cells[sheet] === undefined) cells[sheet] = {};
 
       for (let row = start.row; row <= end.row; row++) {
         for (let col = start.column; col <= end.column; col++) {
           const address = buildCellAddress(col, row, sheet);
           const stringAddress = sf.cellAddressToString(address);
-          cells[sheet].push({
+          cells[sheet][stringAddress] = {
             address,
             stringAddress,
             content: sf.getCell(address),
-          });
+          };
         }
       }
     }
   }
 
+  const finalCells: GroupedCells = {};
+  for (const key of Object.keys(cells)) {
+    finalCells[key] = Object.values(cells[key]);
+  }
+
   return {
-    cells,
-    namedExpressions,
+    cells: finalCells,
+    namedExpressions: Object.values(namedExpressions),
     missingSheets: Array.from(missingSheets),
     missingNamedExpressions: Array.from(missingNamedExpressions),
   };
