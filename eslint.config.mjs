f295a8dfae1b54a13d6151699f43fd3b1e705b36
Patryk Szela <53914832+PatrickSzela@users.noreply.github.com>
2024-11-03 02:49:38 +0100

    build: update dependencies, migrate to Next.js 15

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    ignores: ["!**/.storybook"],
+  },
+  ...compat.extends("next/core-web-vitals", "plugin:storybook/recommended"),
+];
