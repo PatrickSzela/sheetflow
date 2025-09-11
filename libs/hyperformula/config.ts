e4b1ec918c1483f4d116eda1bc2deea7332be306
Patryk Szela <53914832+PatrickSzela@users.noreply.github.com>
2025-09-11 02:48:56 +0200

    refactor: unmapLanguageCode returns lang codes supported by HF

--- libs/hyperformula/config.ts
+++ libs/hyperformula/config.ts
@@ -1,3 +1,4 @@
+import * as Languages from "hyperformula/i18n/languages";
 import type { HyperFormulaConfig } from "@/libs/hyperformula/hyperformulaEngine";
 import type { SheetFlowConfig } from "@/libs/sheetflow";
 
@@ -5,8 +6,16 @@ export const remapLanguageCode = (languageCode: string): string => {
   return [languageCode.slice(0, 2), languageCode.slice(2)].join("-");
 };
 
-export const unmapLanguageCode = (languageCode: string): string => {
-  return languageCode.replace("-", "");
+export const unmapLanguageCode = (languageCode: string) => {
+  const lang = languageCode.replace("-", "");
+
+  if (!(lang in Languages)) {
+    throw new Error(
+      `Language ${languageCode} is not supported by HyperFormula`,
+    );
+  }
+
+  return lang as keyof Omit<typeof Languages, "default">;
 };
 
 export const remapConfig = (config: HyperFormulaConfig): SheetFlowConfig => {
