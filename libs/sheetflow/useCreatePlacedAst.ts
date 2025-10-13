cee0aeb4df72dfe1063d5818c3cc664906b31764
Patryk Szela <53914832+PatrickSzela@users.noreply.github.com>
2025-10-13 21:05:16 +0200

    fix: temporary workaround for Placed AST being created twice in dev mode

--- libs/sheetflow/useCreatePlacedAst.ts
+++ libs/sheetflow/useCreatePlacedAst.ts
@@ -1,8 +1,11 @@
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
+import { PlacedAst } from "./placedAst";
 import { usePlacedAst } from "./usePlacedAst";
 import { useSheetFlow } from "./useSheetFlow";
 
 // TODO: warning when initial args have changed?
+
+let globalPlacedAstTemp: PlacedAst | null;
 
 export const useCreatePlacedAst = (
   initialFormula?: string,
@@ -10,14 +13,19 @@ export const useCreatePlacedAst = (
 ) => {
   const sf = useSheetFlow();
 
-  const [initial] = useState(() => ({
-    formula: initialFormula,
-    scope: initialScope,
-  }));
+  // WORKAROUND: this is a temporary workaround for creation of initial placed ast
+  // until tabbed interface is implemented where multiple placed ast can coexists.
+  // This also means we'll be able to create these in event handlers which will make React very happy :)
+  const [createdAst] = useState<PlacedAst>(() => {
+    globalPlacedAstTemp ??= sf.createPlacedAst(initialFormula, initialScope);
+    return globalPlacedAstTemp;
+  });
 
-  const createdAst = useMemo(() => {
-    return sf.createPlacedAst(initial.formula, initial.scope);
-  }, [initial.formula, initial.scope, sf]);
+  useEffect(() => {
+    return () => {
+      globalPlacedAstTemp = null;
+    }
+  }, []);
 
   return usePlacedAst(createdAst.uuid);
 };
